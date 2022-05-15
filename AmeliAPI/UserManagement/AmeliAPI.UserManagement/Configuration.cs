c0e6e0bc4c1f9cf454f042cf6354277b76b63c4b
cyangrave <cyangrave@gmx.de>
2022-05-15 14:05:09 +0200

    cleanup

---
+++ AmeliAPI/UserManagement/AmeliAPI.UserManagement/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using AmeliAPI.UserManagement.Model.Interfaces;
+using AmeliAPI.UserManagement.Services;
+using Microsoft.Extensions.DependencyInjection;
+using ModelBase;
+
+namespace AmeliAPI.UserManagement
+{
+    public static class Configuration
+    {
+        public static void ConfigureServices(IServiceCollection services, IDataAccessProvider dataAccessProvider)
+        {
+            services.AddSingleton<IUserService>( new UserService(dataAccessProvider));
+        }
+    }
+}
